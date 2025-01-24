363162726cfbdacf4ecf2262542ddccf074ff9fc
koppesan-2 <150250876+koppesan-2@users.noreply.github.com>
2025-01-24 14:12:28 +0900

    上に戻る追加

--- js/anatahe.js
+++ js/anatahe.js
@@ -4,3 +4,6 @@ window.addEventListener("DOMContentLoaded",function(){
 function sentaku(){
     document.getElementById("ongen").scrollIntoView({behavior:"smooth"})
 }
+function gotop(){
+    document.getElementById("sentakubutton").scrollIntoView({behavior:"smooth"})
+}
