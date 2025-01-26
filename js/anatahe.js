e3ea81ca85af9f10ef8ab1ba1106a37b17b70371
koppesan-2 <koppesan@outlook.jp>
2025-01-27 02:26:55 +0900

    メニュー追加

--- js/anatahe.js
+++ js/anatahe.js
@@ -7,3 +7,45 @@ function sentaku(){
 function gotop(){
     document.getElementById("top").scrollIntoView({behavior:"smooth"})
 }
+function getAudioFile(url,id){
+    fetch(url)
+    .then(response => {
+        if(response.ok){
+        return response.blob()
+    }else{
+        throw new Error(`FetchError:${response.status}`);
+    }
+    })
+    .then(blobData => {
+        if(blobData){
+        var blobUrl=URL.createObjectURL(blobData);
+        let audioElement=document.createElement("audio");
+        audioElement.id=id;
+        audioElement.src=blobUrl;
+        audioElement.controls=true
+        const elm=document.getElementById(id);
+        if(elm){
+            throw new Error(`PredefinedID:${id}`);
+        }
+        let setDefaultPlay=document.createElement("button");
+        setDefaultPlay.innerText="設定";
+        setDefaultPlay.id=`${id}change`;
+        setDefaultPlay.setAttribute("onclick","autoclose()");
+        document.getElementById(`${id}div`).appendChild(setDefaultPlay);
+        document.getElementById(`${id}div`).appendChild(document.createElement("br"));
+        document.getElementById(`${id}div`).appendChild(audioElement);
+        document.getElementById(`${id}button`).remove();
+    }
+    })
+    .catch(e => {
+      console.log(e.message);
+        showerror(e.message);
+    })
+}
+function showerror(em){
+    error=em.indexOf("FetchError")!==-1?"データが取得できませんでした":em.indexOf("PredefinedID")!==-1?"このIDはすでに使用されています":"エラーが発生しました";
+
+}
+function autoclose(){
+    document.getElementById("menuswitch").click();
+}
