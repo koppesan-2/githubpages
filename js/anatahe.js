4afa4c2cb4dccfa3bb387bf2b7d11ebce0d74313
koppesan-2 <koppesan@outlook.jp>
2025-03-03 09:57:52 +0900

    音源再読込ボタンの追加とKキーでの再生・停止

--- js/anatahe.js
+++ js/anatahe.js
@@ -56,6 +56,10 @@ function gotop(){
     scroll("top");
 }
 function getAudioFile(url,id){
+    document.getElementById(`${id}button`).style=""
+    if(document.getElementById(id)){document.getElementById(id).remove();}
+    if(document.getElementById(`${id}change`)){document.getElementById(`${id}change`).remove();}
+    if(document.getElementById(`${id}br`)){document.getElementById(`${id}br`).remove();}
     document.getElementById(`${id}button`).innerText="ダウンロード中...";
     fetch(url)
     .then(response => {
@@ -83,9 +87,12 @@ function getAudioFile(url,id){
         setDefaultPlay.setAttribute("onclick",`setDefault('${id}')`);
         setDefaultPlay.setAttribute("class","buttons");
         document.getElementById(`${id}div`).appendChild(setDefaultPlay);
-        document.getElementById(`${id}div`).appendChild(document.createElement("br"));
+        audiobr=document.createElement("br");
+        audiobr.id=`${id}br`;
+        document.getElementById(`${id}div`).appendChild(audiobr);
         document.getElementById(`${id}div`).appendChild(audioElement);
-        document.getElementById(`${id}button`).remove();
+        document.getElementById(`${id}button`).innerHTML='<svg style="transform: translateX(-50%);" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512"><path d="M142.9 142.9c-17.5 17.5-30.1 38-37.8 59.8c-5.9 16.7-24.2 25.4-40.8 19.5s-25.4-24.2-19.5-40.8C55.6 150.7 73.2 122 97.6 97.6c87.2-87.2 228.3-87.5 315.8-1L455 55c6.9-6.9 17.2-8.9 26.2-5.2s14.8 12.5 14.8 22.2l0 128c0 13.3-10.7 24-24 24l-8.4 0c0 0 0 0 0 0L344 224c-9.7 0-18.5-5.8-22.2-14.8s-1.7-19.3 5.2-26.2l41.1-41.1c-62.6-61.5-163.1-61.2-225.3 1zM16 312c0-13.3 10.7-24 24-24l7.6 0 .7 0L168 288c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-41.1 41.1c62.6 61.5 163.1 61.2 225.3-1c17.5-17.5 30.1-38 37.8-59.8c5.9-16.7 24.2-25.4 40.8-19.5s25.4 24.2 19.5 40.8c-10.8 30.6-28.4 59.3-52.9 83.8c-87.2 87.2-228.3 87.5-315.8 1L57 457c-6.9 6.9-17.2 8.9-26.2 5.2S16 449.7 16 440l0-119.6 0-.7 0-7.6z"/></svg>'
+        document.getElementById(`${id}button`).style="transform:translateY(20%);width:40px;height:60px;fill:#fff;"
     }
     })
     .catch(e => {
@@ -125,6 +132,17 @@ function setDefault(id){
     track.connect(audioplaygainnode).connect(audiocontext.destination);
     volumeboostbuttonset();
     autoclose();
+
+document.addEventListener("keydown",event=>{
+    console.log("あ")
+    if(event.code==="KeyK"){
+        if(document.getElementById("playaudio").paused){
+            document.getElementById("playaudio").play()
+        }else{
+            document.getElementById("playaudio").pause()
+        }
+    }
+})
 }
 function volumeboostbuttonset(){
     volumebutton=document.createElement("input");
